e45b7208052bb09d8f6424e4d0f0478b85acba41
IPIvliev <roadnnov@gmail.com>
2013-05-21 10:33:01 +0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NnteDebet::Application.config.secret_token = '7eb9971123245b05774d6644624899a9d3e5f0299720e24a34e1e18a95da1874a6efe407542580b4c5ee69e1703815f907055f0be9cdb26600ce6e40f8d5b3b3'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+NnteDebet::Application.config.session_store :cookie_store, key: '_nnte-debet_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# NnteDebet::Application.config.session_store :active_record_store
