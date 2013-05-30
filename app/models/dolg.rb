af585ff22403a9b165f1d8fad770b27599e30043
IPIvliev <rusroad@gmail.com>
2013-05-30 19:40:18 +0400

    Start work with validates_date

--- app/models/dolg.rb
+++ app/models/dolg.rb
@@ -2,5 +2,7 @@ class Dolg < ActiveRecord::Base
   attr_accessible :dogovor_id, :period, :sum
   belongs_to :dogovor
 
-  validate :validate_created_at
+  validates_date :created_at, :is_at => lambda { 1.day.ago },
+                               :is_at_message => "must be at least 18 years old"
+
 end
